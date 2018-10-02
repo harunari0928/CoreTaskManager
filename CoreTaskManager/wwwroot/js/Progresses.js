9b0710c5a27bda1d45ffb3ba68aebacb910c619e
Harunari <nanteneus@hotmail.co.jp>
2018-10-02 11:34:06 +0900

    進捗一覧で項目を押すとページ遷移する機能実装

---
+++ CoreTaskManager/wwwroot/js/Progresses.js
@@ -0,0 +1,6 @@
+﻿window.onload = () => {
+    $('.card').on('click' , e => {
+        let progressId = e.currentTarget.id;
+        window.location.href = "TaskManager/Index?progressId=" + progressId;
+    });
+}
