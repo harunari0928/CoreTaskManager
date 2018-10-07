a0b80729f68afe1f3c18149a1d0b8f6a5f1a1a62
haruanri <nanteneus@hotmail.co.jp>
2018-10-08 07:43:24 +0900

    不要なusingを削除

--- CoreTaskManager/Pages/Progresses/Index.cshtml.cs
+++ CoreTaskManager/Pages/Progresses/Index.cshtml.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
