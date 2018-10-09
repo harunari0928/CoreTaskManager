a55c20cfa9d2ef7f128ae577817292d68418a070
Harunari <nanteneus@hotmail.co.jp>
2018-10-09 14:49:32 +0900

    管理画面へタスク設定ボタンを追加

--- CoreTaskManager/Pages/OwnerPage/Index.cshtml.cs
+++ CoreTaskManager/Pages/OwnerPage/Index.cshtml.cs
@@ -10,6 +10,7 @@ using CoreTaskManager.Models;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
 
 namespace CoreTaskManager.Pages.OwnerPage
 {
@@ -18,6 +19,7 @@ namespace CoreTaskManager.Pages.OwnerPage
     {
         private readonly CoreTaskManager.Models.CoreTaskManagerContext _context;
         private const string SessionCurrentProgress = "CurrentProgress";
+        private const string SessionNumberOfTasks = "NumberOfTasks";
 
         public IndexModel(CoreTaskManager.Models.CoreTaskManagerContext context)
         {
@@ -25,6 +27,7 @@ namespace CoreTaskManager.Pages.OwnerPage
         }
 
         public IList<AchievedTask> AchievedTasks { get;set; }
+        public Progress ThisProgress { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string progressIdString)
         {
@@ -37,14 +40,15 @@ namespace CoreTaskManager.Pages.OwnerPage
                 return Redirect("../Progresses");
             }
             int currentProgress = int.Parse(progressIdString);
-            var thisProgress = _context.Progresses.Where(p => p.Id == currentProgress).First();
+            var thisProgress = _context.Progresses.Where(p => p.Id == currentProgress);
             // 進捗を作成したユーザとページを開いたユーザが一致しなければ一覧ページへ戻る
-            if (thisProgress.UserName != User.Identity.Name)
+            if (thisProgress.First().UserName != User.Identity.Name)
             {
                 return Redirect("../Progresses");
             }
             HttpContext.Session.SetInt32(SessionCurrentProgress, currentProgress);
             AchievedTasks = await _context.AchievedTasks.ToListAsync();
+            ThisProgress = await thisProgress.FirstAsync();
             return Page();
         }
         public JsonResult OnPostSendUnappliedTable()
@@ -77,6 +81,68 @@ namespace CoreTaskManager.Pages.OwnerPage
             {
                 return new JsonResult("serverError");
             }
+        }
+        public ActionResult OnPostSetTasks()
+        {
+            if (!HttpContext.Session.IsAvailable)
+            {
+                HttpContext.Session.LoadAsync();
+            }
+            try
+            {
+                int? progressId = HttpContext.Session.GetInt32(SessionCurrentProgress);
+                int? numberOfTasks = HttpContext.Session.GetInt32(SessionNumberOfTasks);
+                if (progressId == null || numberOfTasks == null)
+                {
+                    return new JsonResult("serverError");
+                }
+
+                var Tasks = new List<TaskModel>();
+                {
+                    var stream = new MemoryStream();
+                    Request.Body.CopyTo(stream);
+                    stream.Position = 0;
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string requestBody = reader.ReadToEnd();
+                        if (requestBody.Length > 0)
+                        {
+                            var receiveData = JsonConvert.DeserializeObject<Dictionary<string, string>>(requestBody);
+                            for (int i = 1; i <= receiveData.Count; i++)
+                            {
+                                var receiveString = receiveData[$"task{i.ToString()}"];
+                                // すでにタスクが登録されていた場合は登録できない
+                                if (numberOfTasks > 0)
+                                {
+                                    return new JsonResult("wrongString");
+                                }
+                                if (receiveString.Length > 15 || receiveString.Length == 0)
+                                {
+                                    return new JsonResult("wrongString");
+                                }
+
+                                Tasks.Add(new TaskModel
+                                {
+                                    ProgressId = (int)progressId,
+                                    TaskName = receiveString
+                                });
+                            }
+                            ThisProgress = _context.Progresses.FirstOrDefault(p => p.Id == progressId);
+                            ThisProgress.NumberOfItems = receiveData.Count;
+                        }
+                    }
+                }
+
+                Tasks.ForEach(task => _context.Tasks.Add(task));
+                _context.SaveChanges();
+                Redirect($"TaskManager/Index?progressIdString={progressId}");
+                return new JsonResult("success");
+            }
+            catch
+            {
+                return new JsonResult("serverError");
+            }
+
         }
     }
 }
--- CoreTaskManager/wwwroot/js/OwnerPage.js
+++ CoreTaskManager/wwwroot/js/OwnerPage.js
@@ -1,10 +1,16 @@
 ﻿window.onload = () => {
     let utt = new UnappliedTasksTable();
+    let opTask = new OperateTaskForm();
 }
 
 class UnappliedTasksTable {
     constructor() {
         this.requestTableObject();
+        $('h3').on('click', e => {
+            console.log(e);
+            $('#aSingleWord').text = $('#words' + e.target.id).val();
+            $('#approval').modal('show');
+        });
     }
 
     requestTableObject() {
@@ -44,7 +50,6 @@ class UnappliedTasksTable {
             "<th scope='col'>題名</th>" +
             "<th scope='col'>タスク名</th>" +
             "<th scope='col'>申請日時</th>" +
-            "<th scope='col'>一言</th>" +
             "</tr>" +
             "</thead>" +
             "<tbody>";
@@ -55,8 +60,9 @@ class UnappliedTasksTable {
                 "<td>" + tasksObject[i].progressName + "</td>" +
                 "<td>" + tasksObject[i].taskName + "</td>" +
                 "<td>" + tasksObject[i].achievedDateTime + "</td>" +
-                "<td>" + tasksObject[i].description + "</td>" +
-                "</tr>";
+                "<input id='words" + tasksObject[i].achivedTaskId + "' type='hidden' value=" + tasksObject[i].description + "/>" +
+                "</tr>"
+                ;
         }
         tag += "" +
             "</tbody>" +
@@ -64,5 +70,138 @@ class UnappliedTasksTable {
 
         let resultTag = document.getElementById("unappliedTaskTable");
         resultTag.innerHTML = tag;
+    }
+}
+
+class OperateTaskForm {
+    constructor() {
+        // 初めはフォーム一つだけ
+        this.countTask = 1;
+
+        $('#addTask').on('click', e => {
+            this.addTaskForm();
+        });
+        $('#removeTask').on('click', e => {
+            this.removeTaskForm();
+        });
+        $('#modalClose').on('click', e => {
+            this.destroyFirstForms();
+            this.destroyAddedForms();
+        });
+        $('#transmitTaskData').on('click', e => {
+            if (!this.isCaughtValidate()) {
+                if (this.transmitTaskData() === "success") {
+                    this.destroyFirstForms();
+                    this.destroyAddedForms();
+                    $('.modal-backdrop').remove(); 
+                    $('#registerNew').modal('hide');
+                    $('#modalButton').hide();
+                }
+            }
+        });
+    }
+
+    addTaskForm() {
+        if (this.countTask > 99) {
+            this.countTask = 99;
+        }
+        this.countTask++;
+        let tag;
+        let divElement = document.createElement("div");
+        divElement.setAttribute('class', 'form-group');
+        divElement.setAttribute('id', this.countTask);
+
+        tag = "" +
+            "<div class='alert1' id='task" + this.countTask + "Alert'></div>" +
+            "<label>・タスク" + this.countTask + "(15文字以内)</label>" +
+            "<input type='text' id='task" + this.countTask + "' class='form-control' />";
+
+        divElement.innerHTML = tag;
+        let parentObject = document.getElementById("addedTask");
+        parentObject.appendChild(divElement);
+    }
+    removeTaskForm() {
+        if (this.countTask > 1) {
+            let parentObject = document.getElementById("addedTask");
+            let targetElement = document.getElementById(this.countTask);
+            parentObject.removeChild(targetElement);
+            this.countTask--;
+        }
+    }
+    destroyFirstForms() {
+        $('#task1').val('');
+        $('#task1Alert').text('');
+    }
+    destroyAddedForms() {
+        let addedTask = document.getElementById("addedTask");
+        for (let i = 2; i <= this.countTask; i++) {
+            let targetElement = document.getElementById(i);
+            addedTask.removeChild(targetElement);
+        }
+        this.countTask = 1;
+    }
+    isCaughtValidate() {
+        let catchValidateFlag = false;
+        if ($('#task1').val() === "") {
+            $('#task1Alert').text("*入力してください");
+            catchValidateFlag = true;
+        } else if ($('#task1').val().length > 15) {
+            $('#task1Alert').text("*15文字以内で入力してください");
+            catchValidateFlag = true;
+        }
+        let addedTask = document.getElementById("addedTask");
+        for (let i = 2; i <= this.countTask; i++) {
+            let inputStr = addedTask.childNodes[i - 2].childNodes[2];
+            let alertElement = addedTask.childNodes[i - 2].childNodes[0];
+            if (inputStr.value === "") {
+                alertElement.textContent = "*入力してください";
+                catchValidateFlag = true;
+            } else if (inputStr.value.length > 15) {
+                alertElement.textContent = "*15文字以内で入力してください";
+                catchValidateFlag = true;
+            }
+        }
+        return catchValidateFlag;
+    }
+    transmitTaskData() {
+        // 入力された文字列（タスク名）取得
+        let tasks = { task1: $('#task1').val() };
+        let addedTask = document.getElementById("addedTask");
+        for (var i = 2; i <= this.countTask; i++) {
+            let formElement = addedTask.childNodes[i - 2].childNodes[2];
+            tasks[formElement.id] = formElement.value;
+        }
+
+        let transmitSuccessFlag = true;
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(tasks),
+            url: "/TaskManager?handler=SetTasks",
+            beforeSend: xhr => {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: response => {
+                if (response === "serverError") {
+                    alert("サーバでエラーが発生しました");
+                    transmitSuccessFlag = false;
+                }
+                if (response === "wrongString") {
+                    alert("不正な値が入力されました");
+                    transmitSuccessFlag = false;
+                }
+            },
+            failure: response => {
+                alert("通信に失敗しました");
+                transmitSuccessFlag = false;
+            }
+        });
+        if (transmitSuccessFlag) {
+            return "success";
+        } else {
+            return "failed";
+        }
     }
 }
