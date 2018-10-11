5835104ce8ddb46b120cff81c60568f50b6c484a
haruanri <nanteneus@hotmail.co.jp>
2018-10-11 22:25:48 +0900

    ローディング後、表示対象がフェードインするように修正

--- CoreTaskManager/wwwroot/js/site.js
+++ CoreTaskManager/wwwroot/js/site.js
@@ -2,6 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-Pace.on('done', () => {
-            $('.wrap').fadeIn();
-        });
+window.onload = () => {
+    Pace.on('done', () => {
+        $('.wrap').fadeIn();
+    });
+};
+
