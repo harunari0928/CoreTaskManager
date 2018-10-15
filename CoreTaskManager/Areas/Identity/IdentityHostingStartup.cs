63afc06c63c2f420c0dc10c916349a3fec99c160
haruanri <nanteneus@hotmail.co.jp>
2018-10-15 22:09:13 +0900

    アカウント登録ページスキャフォールディング

---
+++ CoreTaskManager/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreTaskManager.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoreTaskManager.Areas.Identity.IdentityHostingStartup))]
+namespace CoreTaskManager.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
