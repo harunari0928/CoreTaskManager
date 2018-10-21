0ba36637ec7b7bd832acdd6f032a939c7d65cc16
haruanri <nanteneus@hotmail.co.jp>
2018-10-21 23:57:59 +0900

    ユニットテスト追加

---
+++ CoreTaskmanagerUnitTest/CoreTaskmanagerUnitTest.cs
@@ -0,0 +1,26 @@
+using CoreTaskManager.Models;
+using CoreTaskManager.Pages.OwnerPage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web.Mvc;
+using Xunit;
+
+
+namespace CoreTaskmanagerUnitTest
+{
+    public class CoreTaskmanagerUnitTest
+    {
+        [Fact]
+        public void SendBackCorrectData_WhenRequestTable()
+        {
+            //using (var db = new CoreTaskManagerContext(Utilities.TestDbContextOptions()))
+            //{
+            //    // arrange
+            //    var page = new IndexModel(db);
+
+            //    // act
+                
+            //}
+        }
+    }
+}
